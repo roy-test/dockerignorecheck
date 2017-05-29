84f840876587ab4f16b932bdeed775b38a71faf0
Roy Shmueli <roy@codefresh.io>
2017-05-29 13:31:06 +0300

    Update docker_wait_postgres.sh

--- docker_wait_postgres.sh
+++ docker_wait_postgres.sh
@@ -1,17 +1,1 @@
-#!/usr/bin/env bash
-
-function test_postgresql {
-  pg_isready -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}"
-}
-
-count=0
-until ( test_postgresql )
-do
-  ((count++))
-  if [ ${count} -gt 100 ]
-  then
-    echo "Services didn't become ready in time"
-    exit 1
-  fi
-  sleep 1
-done
+Hello World
